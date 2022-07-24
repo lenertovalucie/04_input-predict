660abb3db62d274aacb113286afb4e49a3aaa98a
Lucie Lénertová <lenertova.lucie@gmail.com>
2022-07-24 19:25:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# napište předpověď toho, co se stane, až se program spustí (do komentářů pod každý příkaz)
+print("Hello! What's your name?")
+#
+name = input()
+#
+print(name)
+#
+print("Hi " + name + "! How are you today?")
+#
